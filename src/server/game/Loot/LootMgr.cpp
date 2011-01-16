51ab0bb4836d4c6177d49098557dedd3e99832a3
mrhooha <nonya@effing_beeezwax.gtfo>
2011-01-16 12:25:37 -0600

    Ported over a fix from cactus. Fix structure of SMSG_LOOT_RESPONSE. Note: now loot windows open correctly. Patch by Kierkegaard.
    Signed-off-by: mrhooha <nonya@effing_beeezwax.gtfo>

--- src/server/game/Loot/LootMgr.cpp
+++ src/server/game/Loot/LootMgr.cpp
@@ -821,6 +821,7 @@ ByteBuffer& operator<<(ByteBuffer& b, LootView const& lv)
 
     size_t count_pos = b.wpos();                            // pos of item count byte
     b << uint8(0);                                          // item count placeholder
+	b << uint8(0);                                          // unk 4.0.x
 
     switch (lv.permission)
     {
