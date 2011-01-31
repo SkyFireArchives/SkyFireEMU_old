a45ecf74096107879d8fa647ba2429517de7334e
AlterEgo <nonya@effing_beeezwax.gtfo>
2011-01-31 14:35:16 -0600

    Fixed the Windows AutoBuild. Only supports Visual studio 2010
    Signed-off-by: AlterEgo <nonya@effing_beeezwax.gtfo>

--- SkyFire_AutoBuild.bat
+++ SkyFire_AutoBuild.bat
@@ -1,21 +1,18 @@
 REM SkyFire AutoBuild System. Version 1.0
 @echo OFF
 CLS
-REM Compiler Version: Visual Studio 9 2008 = "2008" , Visual Studio 10 = "2010"
-set compiler_ver=2008
+REM Compiler Version: Visual Studio 10 = "2010"
+set compiler_ver=2010
 
 REM Visual Studio build configuration: Release (default), MinSizeRel, RelWithDebInfo or Debug
 set build=Release
 
-REM Select cMake generator: "Visual Studio 10" (32Bit), "Visual Studio 10 Win64" (64Bit) "Visual Studio 9 2008" (32Bit), "Visual Studio 9 2008 Win64" (64Bit)
-set cMake_Generator="Visual Studio 9 2008"
-
-REM Repo
-set repo_git=git@git.assembla.com:skyfire_cactus.git
+REM Select cMake generator: "Visual Studio 10" (32Bit), "Visual Studio 10 Win64" (64Bit)
+set cMake_Generator="Visual Studio 10"
 
 REM Folders
-set f_sources=C:\MyAss\SkyFire
-set f_cmake=C:\MyAss\SkyFire_Builds
+set f_sources=.
+set f_cmake=.
 set f_compile_output=%f_cmake%\bin\Release\
 
 REM Checking for 32/64Bit OS
@@ -27,42 +24,13 @@ set Git="%prog_path%\Git\bin\git.exe"
 set CMake="%prog_path%\CMake 2.8\bin\cmake.exe"
 
 REM VisualStudio Path
-set VisualStudio_2008="%prog_path%\Microsoft Visual Studio 9.0\Common7\IDE\devenv.exe"
 set VisualStudio_2010="%prog_path%\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe"
 
 echo ###############################################################################
 echo ###                     SkyFire AutoBuild Vesion 1.0                        ###
 echo ###############################################################################
 echo.
-echo.
-echo 1. Clone/Clean/Pull Repos ###
-echo ###############################################################################
-
-echo.
-IF not exist %f_sources% (
-echo ### Clone SkyFire
-echo.
-echo - Create folder "%f_sources%"
-echo.
-md %f_sources%
-echo.
-call %Git% clone %repo_git% %f_sources%
-)ELSE (
-cd %f_sources%
-echo.
-echo ### Reset SkyFire to local "origin/master"
-echo.
-call %Git% reset --hard "origin/master"
-echo.
-echo.
-echo ### Pull SkyFire to latest version
-echo.
-call %Git% pull
-echo.
-)
-
-echo.
-echo 3. cMake ###
+echo 1. cMake ###
 echo ###############################################################################
 echo.
 
@@ -85,17 +53,17 @@ call %CMAKE% -G %cMake_Generator% %f_sources%
 echo.
 
 echo.
-echo 4. Compile SkyFire with VisualStudio CommandLine ###
+echo 2. Compile SkyFire with VisualStudio CommandLine ###
 echo ###############################################################################
 echo.
 
 echo - This will take some minutes...
 echo - Get a coffee, check your mail, take a shit...
 echo - You will not see any messages until finished...
-call %VisualStudio_2008% /out "%f_compile_output%build_%Date%.txt" /rebuild %build% "%f_cmake%\CactusEMU.sln"
+call %VisualStudio_2010% /out "%f_compile_output%build_%Date%.txt" /rebuild %build% "%f_cmake%\CactusEMU.sln"
 
 echo.
-echo 6. Build SkyFire finished ###
+echo 3. Build SkyFire finished ###
 echo ###############################################################################
 echo.
 
