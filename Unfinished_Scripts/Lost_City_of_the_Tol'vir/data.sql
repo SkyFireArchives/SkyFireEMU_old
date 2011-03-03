a57292fca5545c224b95024088e6c2a46d1bef09
AlterEgo <admin@projectskyfire.org>
2011-03-03 07:23:26 -0600

    Added some unfinished scripts so I can work on it later.
    Signed-off-by: AlterEgo <admin@projectskyfire.org>

---
+++ Unfinished_Scripts/Lost_City_of_the_Tol'vir/data.sql
@@ -0,0 +1,4 @@
+-- Tol'vir Land Mine - 44840
+
+UPDATE `creature_template` SET `ScriptName`='boss_general_husam' WHERE `entry`=44577; 
+UPDATE `creature_template` SET `ScriptName`='npc_trap' WHERE `entry`=44840; 
