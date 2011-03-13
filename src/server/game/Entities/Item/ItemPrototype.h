909d3a757f6462d8c0420c5ed7515861cf059eed
Hammerstorm <wartactics00@gmail.com>
2011-03-13 18:30:29 -0500

    Fixed proper FindEquipSlot on relics from n0n4m3's commit according to:
    http://www.wowwiki.com/Relic
    
    "In Cataclysm the relic system is simplified. Instead of four different types of relics, each one only usable by one class, all relics will be usable by any class capable of using a relic.
    
    While the items may contain the words Idol, Libram, Totem, or Sigil in their name, they will not be sub-categorized as them. "

--- src/server/game/Entities/Item/ItemPrototype.h
+++ src/server/game/Entities/Item/ItemPrototype.h
@@ -385,10 +385,10 @@ enum ItemSubclassArmor
     ITEM_SUBCLASS_ARMOR_PLATE                   = 4,
     ITEM_SUBCLASS_ARMOR_BUCKLER                 = 5,
     ITEM_SUBCLASS_ARMOR_SHIELD                  = 6,
-    ITEM_SUBCLASS_ARMOR_LIBRAM                  = 7,
-    ITEM_SUBCLASS_ARMOR_IDOL                    = 8,
-    ITEM_SUBCLASS_ARMOR_TOTEM                   = 9,
-    ITEM_SUBCLASS_ARMOR_SIGIL                   = 10,
+    //ITEM_SUBCLASS_ARMOR_LIBRAM                  = 7,
+    //ITEM_SUBCLASS_ARMOR_IDOL                    = 8,
+    //ITEM_SUBCLASS_ARMOR_TOTEM                   = 9,
+    //ITEM_SUBCLASS_ARMOR_SIGIL                   = 10,
     ITEM_SUBCLASS_ARMOR_RELIC                   = 11
 };
  
