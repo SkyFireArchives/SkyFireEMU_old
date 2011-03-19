40a32a5a35735de29f6b330dc333b09a96b5e5a4
AriDEV <michalik.andrej@centrum.sk>
2011-03-20 02:53:31 +0800

    Core/Opcode: SMSG_UPDATE_COMBO_POINTS Thx To Fabian For The Research
    Signed-off-by: AriDEV <michalik.andrej@centrum.sk>

--- src/server/game/Server/Protocol/Opcodes.h
+++ src/server/game/Server/Protocol/Opcodes.h
@@ -923,7 +923,7 @@ enum Opcodes
     CMSG_CHEAT_DUMP_ITEMS_DEBUG_ONLY                 = 0x1015F, //
     SMSG_CHEAT_DUMP_ITEMS_DEBUG_ONLY_RESPONSE        = 0x10160, //
     SMSG_CHEAT_DUMP_ITEMS_DEBUG_ONLY_RESPONSE_WRITE_FILE  = 0x10161, //
-    SMSG_UPDATE_COMBO_POINTS                         = 0x10162, //
+    SMSG_UPDATE_COMBO_POINTS                         = 0x037A8, // 4.0.6a 13623
     SMSG_VOICE_SESSION_ROSTER_UPDATE                 = 0x0ACC0, // 4.0.6a 13623
     SMSG_VOICE_SESSION_LEAVE                         = 0x078A4, // 4.0.6a 13623
     SMSG_VOICE_SESSION_ADJUST_PRIORITY               = 0x10165, //
