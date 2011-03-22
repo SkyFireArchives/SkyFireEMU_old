64a0142cc8a3480f19365529bfdd6e2e443b9c0d
AlterEgo <admin@projectskyfire.org>
2011-03-22 02:38:47 -0500

    Core/Players: Fixed naked bug for players, original idea by BThallid Ported from TrinityCore.
    Signed-off-by: AlterEgo <admin@projectskyfire.org>

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -2063,34 +2063,13 @@ bool Player::TeleportTo(uint32 mapid, float x, float y, float z, float orientati
             if (!GetSession()->PlayerLogout())
             {
                 // send transfer packets
-                WorldPacket data(SMSG_TRANSFER_PENDING, (4+4+4));
+                WorldPacket data(SMSG_TRANSFER_PENDING, (4 + 4 + 4));
                 data << uint32(mapid);
                 if (m_transport)
-                {
                     data << m_transport->GetEntry() << GetMapId();
-                }
-                GetSession()->SendPacket(&data);
 
-                data.Initialize(SMSG_NEW_WORLD, (20));
-                if (m_transport)
-                {
-                    data << uint32(mapid);
-                    data << float(m_movementInfo.t_pos.GetOrientation());
-                    data << float(m_movementInfo.t_pos.GetPositionX());
-                    data << float(m_movementInfo.t_pos.GetPositionY());
-                    data << float(m_movementInfo.t_pos.GetPositionZ());
-                }
-                else
-                {
-                    data << uint32(mapid);
-                    data << float(orientation);
-                    data << float(x);
-                    data << float(y);
-                    data << float(z);
-                }
-                
                 GetSession()->SendPacket(&data);
-                SendSavedInstances();
+               
             }
 
             // remove from old map now
@@ -2115,6 +2094,19 @@ bool Player::TeleportTo(uint32 mapid, float x, float y, float z, float orientati
             SetFallInformation(0, final_z);
             // if the player is saved before worldportack (at logout for example)
             // this will be used instead of the current location in SaveToDB
+
+			if (!GetSession()->PlayerLogout())
+            {
+                WorldPacket data(SMSG_NEW_WORLD, 4 + 4 + 4 + 4 + 4);
+                data << uint32(mapid);
+                if (m_transport)
+                    data << m_movementInfo.t_pos.PositionXYZOStream();
+                else
+                    data << m_teleport_dest.PositionXYZOStream();
+
+                GetSession()->SendPacket(&data);
+                SendSavedInstances();
+            }
 
             // move packet sent by client always after far teleport
             // code for finish transfer to new map called in WorldSession::HandleMoveWorldportAckOpcode at client packet
