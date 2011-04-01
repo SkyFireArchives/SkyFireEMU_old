82eed5ae2e4f844d4906b7434726c7cbac3342cc
AlterEgo <admin@projectskyfire.org>
2011-04-01 15:32:03 -0500

    APPLY SQL UPDATE! Added some missing sql updates for commands. Also made .opcode an administrator only function.
    Signed-off-by: AlterEgo <admin@projectskyfire.org>

---
+++ sql/updates/616_command_updates.sql
@@ -0,0 +1,2 @@
+INSERT INTO `command` VALUES ('lookup npc', '3', 'Syntax: .lookup npc $namepart\r\n\r\nLooks up a npc by $namepart, and returns all matches with their npc ID\'s.');
+INSERT INTO `command` VALUES ('opcode', '4', 'Syntax: .opcode is a developer function not intended for use only for opcode research.);
