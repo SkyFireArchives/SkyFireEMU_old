79769dbd346405398e0db2438d8434a31004136f
Viste <viste02@gmail.com>
2011-04-08 01:16:50 +0800

    Core/Spells: Brain Freeze proc fix

---
+++ sql/custom/world_galifrey.sql
@@ -0,0 +1,8 @@
+-- Brain Freeze
+DELETE FROM `spell_proc_event` WHERE `entry` IN (44546);
+INSERT INTO `spell_proc_event` VALUES (44546, 0x10, 0x03, 0x00000020, 0x00000000, 0x00000000, 0x00010000, 0x00000000, 0, 5, 0);
+DELETE FROM `spell_proc_event` WHERE `entry` IN (44548);
+INSERT INTO `spell_proc_event` VALUES (44548, 0x10, 0x03, 0x00000020, 0x00000000, 0x00000000, 0x00010000, 0x00000000, 0, 10, 0);
+DELETE FROM `spell_proc_event` WHERE `entry` IN (44549);
+INSERT INTO `spell_proc_event` VALUES (44549, 0x10, 0x03, 0x00000020, 0x00000000, 0x00000000, 0x00010000, 0x00000000, 0, 15, 0);
+
