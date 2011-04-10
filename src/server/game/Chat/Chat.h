9f8a8e8fbdb2bd62f7d7ca9de8392dbf78778a55
keany <keany@legendarybrothers.cz>
2011-04-10 21:20:04 +0800

    Core/Scripts/SmartAI: removed test code
    Core/CreatureTextMgr: added reload command
    handle text from DB
    handle grouped texts
    handle repeatable/single texts based on probability
    todo: sound/emote, code cleanup

--- src/server/game/Chat/Chat.h
+++ src/server/game/Chat/Chat.h
@@ -454,6 +454,7 @@ class ChatHandler
         bool HandleReloadAuctionsCommand(const char* args);
         bool HandleReloadWpScriptsCommand(const char* args);
         bool HandleReloadConditions(const char* args);
+        bool HandleReloadCreatureText(const char* args);
 
         bool HandleResetAchievementsCommand(const char * args);
         bool HandleResetAllCommand(const char * args);
--- src/server/game/Chat/Commands/Level3.cpp
+++ src/server/game/Chat/Commands/Level3.cpp
@@ -63,6 +63,7 @@
 #include "WeatherMgr.h"
 #include "ScriptMgr.h"
 #include "LFGMgr.h"
+#include "CreatureTextMgr.h"
 
 //reload commands
 bool ChatHandler::HandleReloadAllCommand(const char*)
@@ -1170,6 +1171,14 @@ bool ChatHandler::HandleReloadConditions(const char* /*args*/)
     sLog.outString("Re-Loading Conditions...");
     sConditionMgr.LoadConditions(true);
     SendGlobalGMSysMessage("Conditions reloaded.");
+    return true;
+}
+
+bool ChatHandler::HandleReloadCreatureText(const char* /*args*/)
+{
+    sLog.outString("Re-Loading Creature Texts...");
+    sCreatureTextMgr.LoadCreatureTexts();
+    SendGlobalGMSysMessage("Creature Texts reloaded.");
     return true;
 }
 
