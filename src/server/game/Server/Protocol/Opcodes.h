0b7bdf8a0d142e3f28304a4625f2ba9675c72356
AlterEgo <admin@projectskyfire.org>
2011-04-10 14:09:51 -0500

    Fixed conflicting opcode that broke the barber chairs. Will need to find the correct opcode for SMSG_CAMERA_SHAKE.
    Signed-off-by: AlterEgo <admin@projectskyfire.org>

--- src/server/game/Server/Protocol/Opcodes.h
+++ src/server/game/Server/Protocol/Opcodes.h
@@ -1268,7 +1268,7 @@ enum Opcodes
     SMSG_CORPSE_NOT_IN_INSTANCE                      = 0x031E4, // 4.0.6a 13623
     CMSG_SET_ALLOW_LOW_LEVEL_RAID1                   = 0x035EC, // 4.0.6a 13623
     CMSG_SET_ALLOW_LOW_LEVEL_RAID2                   = 0x029C4, // 4.0.6a 13623
-    SMSG_CAMERA_SHAKE                                = 0x034A4, // 4.0.6a 13623
+    SMSG_CAMERA_SHAKE                                = 0x1000F,
     SMSG_UPDATE_ITEM_ENCHANTMENTS                    = 0x020E8, // 4.0.6a 13623
     SMSG_REDIRECT_CLIENT                             = 0x0201B, // 4.0.6a 13623
     CMSG_REDIRECTION_FAILED                          = 0x1000F,
