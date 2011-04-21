3a0213a53ca3394915d7554d19aca324c422d355
Remake <havoc.remake@gmail.com>
2011-04-21 20:23:53 +0200

    Core/Scripts: Add Lost City of Tol'vir header file to the unfinished scripts. Thanks Zenyth.

---
+++ Unfinished_Scripts/Lost_City_of_the_Tol'vir/lost_city_of_the_tolvir.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2010-2011 SkyFire <http://www.projectskyfire.org/>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef DEF_LOST_CITY_OF_THE_TOLVIR_H
+#define DEF_LOST_CITY_OF_THE_TOLVIR_H
+
+#define LCTSCriptName "instance_lost_city_of_the_tolvir"
+
+enum eData
+{
+	DATA_GENERAL_HUSAM_EVENT,
+	DATA_HIGH_PROPHET_BARIM_EVENT,
+	DATA_LOCKMAW_AUCH_EVENT,
+	DATA_SIAMAT_EVENT,
+	DATA_TEAM_IN_INSTANCE
+};
+enum eData64
+{
+	DATA_GENERAL_HUSAM,
+	DATA_LOCKMAW,
+	DATA_AUCH,
+	DATA_HIGH_PROPHET_BARIM,
+	DATA_SIAMAT
+};
+
+#endif
