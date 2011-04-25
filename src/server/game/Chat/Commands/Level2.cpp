b1a561d3a0da1a497e22e8ffe67282c6f130d4ed
Skyfire <admin@projectskyfire.org>
2011-04-24 19:50:14 -0500

    [789] Found another error. Cerey please test this stuff before u commit.
    Signed-off-by: Skyfire <admin@projectskyfire.org>

--- src/server/game/Chat/Commands/Level2.cpp
+++ src/server/game/Chat/Commands/Level2.cpp
@@ -1987,8 +1987,8 @@ bool ChatHandler::HandleKickPlayerCommand(const char *args)
     if (HasLowerSecurity(target, 0))
         return false;
 
-    if (sWorld.getBoolConfig(CONFIG_SHOW_KICK_IN_WORLD))
-        sWorld.SendWorldText(LANG_COMMAND_KICKMESSAGE, playerName.c_str());
+    if (sWorld->getBoolConfig(CONFIG_SHOW_KICK_IN_WORLD))
+        sWorld->SendWorldText(LANG_COMMAND_KICKMESSAGE, playerName.c_str());
     else
         PSendSysMessage(LANG_COMMAND_KICKMESSAGE, playerName.c_str());
 
