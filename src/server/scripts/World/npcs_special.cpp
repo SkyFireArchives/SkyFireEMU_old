3f2a5ac4a7fa0f7772404f012b254cfab3052f25
Remake <havoc.remake@gmail.com>
2011-04-25 18:52:26 +0200

    Core/Spells: Fix typo

--- src/server/scripts/World/npcs_special.cpp
+++ src/server/scripts/World/npcs_special.cpp
@@ -2770,7 +2770,7 @@ public:
 
             if (uiDespawnTimer <= diff)
             {
-                // me->SetVisibility(VISIBILITY_OFF);
+                me->SetVisible(false);
                 me->DisappearAndDie();
             }
             else
