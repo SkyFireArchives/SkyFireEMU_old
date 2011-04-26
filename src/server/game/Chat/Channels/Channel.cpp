7460064f159759b8944ea0685a9bbe61caf32730
Cerey <thyros82@gmx.de>
2011-04-26 21:19:15 +0200

    [794] Core/Disables: Add flags SPELL_DISABLE_MAP and SPELL_DISABLE_AREA for sourceType DISABLE_TYPE_SPELL. This will allow you to disable spells on certain maps/areas. Note that you must have at least flag SPELL_DISABLE_PLAYER or SPELL_DISABLE_CREATURE or SPELL_DISABLE_PET set too, as they take preference. The parameters for mapIds and areaIds are defined in new columns params_0 and params_1 respectively, in a comma-seperated string.
    Example:
    INSERT INTO disables VALUES(0, 8921, (1+16+32), "571,1", "1519", "Moonfire Example");
    
    This will disable spell moonfire for players in maps 571,1 and area 1519.

--- src/server/game/Chat/Channels/Channel.cpp
+++ src/server/game/Chat/Channels/Channel.cpp
@@ -93,7 +93,7 @@ Channel::Channel(const std::string& name, uint32 channel_id, uint32 Team)
                 stmt->setString(0, name);
                 stmt->setUInt32(1, m_Team);
                 CharacterDatabase.Execute(stmt);
-                sLog.outDebug("Channel(%s) saved in database", name.c_str());
+                sLog->outDebug("Channel(%s) saved in database", name.c_str());
             }
 
             m_IsSaved = true;
@@ -121,7 +121,7 @@ void Channel::UpdateChannelInDB() const
         stmt->setUInt32(5, m_Team);
         CharacterDatabase.Execute(stmt);
 
-        sLog.outDebug("Channel(%s) updated in database", m_name.c_str());
+        sLog->outDebug("Channel(%s) updated in database", m_name.c_str());
     }
 
 }
@@ -139,7 +139,7 @@ void Channel::CleanOldChannelsInDB()
     if (sWorld->getIntConfig(CONFIG_PRESERVE_CUSTOM_CHANNEL_DURATION) > 0)
     {
         PreparedStatement* stmt = CharacterDatabase.GetPreparedStatement(CHAR_CLEAN_CHANNEL);
-        stmt->setUInt32(0, sWorld.getIntConfig(CONFIG_PRESERVE_CUSTOM_CHANNEL_DURATION)*DAY);
+        stmt->setUInt32(0, sWorld->getIntConfig(CONFIG_PRESERVE_CUSTOM_CHANNEL_DURATION)*DAY);
         CharacterDatabase.Execute(stmt);
 
         sLog->outDebug("Cleaned out unused custom chat channels.");
