41bc7eb9cb689ba6260b17f73816da58141576a5
Rad3k <rad3k.t@seznam.cz>
2011-04-29 18:54:46 +0200

    Again clean up some spaces (bad editor)

--- src/server/game/Entities/Unit/StatSystem.cpp
+++ src/server/game/Entities/Unit/StatSystem.cpp
@@ -1194,7 +1194,7 @@ void Guardian::UpdateMaxHealth()
         case ENTRY_SUCCUBUS:    multiplicator = 9.1f;   break;
         case ENTRY_FELHUNTER:   multiplicator = 9.5f;   break;
         case ENTRY_FELGUARD:    multiplicator = 11.0f;  break;
-	 case ENTRY_BLOODWORM:   multiplicator = 1.0f;   break;
+		case ENTRY_BLOODWORM:   multiplicator = 1.0f;   break;
         default:                multiplicator = 10.0f;  break;
     }
 
