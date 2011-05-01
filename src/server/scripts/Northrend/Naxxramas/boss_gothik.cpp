471e72de0e02e858e739226471f46102a652085f
Rad3k <rad3k.t@seznam.cz>
2011-05-01 12:08:49 +0200

    Fix gothik adds spawn

--- src/server/scripts/Northrend/Naxxramas/boss_gothik.cpp
+++ src/server/scripts/Northrend/Naxxramas/boss_gothik.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008-2010 TrinityCore <http://www.trinitycore.org/>
+ * Copyright (C) 2008-2011 TrinityCore <http://www.trinitycore.org/>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
@@ -172,7 +172,7 @@ public:
             LiveTriggerGUID.clear();
             DeadTriggerGUID.clear();
 
-            me->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_OOC_NOT_ATTACKABLE|UNIT_FLAG_DISABLE_MOVE);
+            me->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_OOC_NOT_ATTACKABLE|UNIT_FLAG_DISABLE_MOVE|UNIT_FLAG_NOT_SELECTABLE);
             me->SetReactState(REACT_PASSIVE);
             if (instance)
                 instance->SetData(DATA_GOTHIK_GATE, GO_STATE_ACTIVE);
@@ -199,7 +199,7 @@ public:
             }
 
             _EnterCombat();
-            me->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_OOC_NOT_ATTACKABLE|UNIT_FLAG_DISABLE_MOVE);
+            me->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_OOC_NOT_ATTACKABLE|UNIT_FLAG_DISABLE_MOVE|UNIT_FLAG_NOT_SELECTABLE);
             waveCount = 0;
             events.ScheduleEvent(EVENT_SUMMON, 30000);
             DoTeleportTo(PosPlatform);
@@ -248,60 +248,27 @@ public:
 
         void DoGothikSummon(uint32 entry)
         {
-            if (getDifficulty() == RAID_DIFFICULTY_25MAN_NORMAL)
+            switch(entry)
             {
-                switch(entry)
+                case MOB_LIVE_TRAINEE:
                 {
-                    case MOB_LIVE_TRAINEE:
-                    {
-                        if (Creature *LiveTrigger0 = Unit::GetCreature(*me, LiveTriggerGUID[0]))
-                            DoSummon(MOB_LIVE_TRAINEE, LiveTrigger0, 1);
-                        if (Creature *LiveTrigger1 = Unit::GetCreature(*me, LiveTriggerGUID[1]))
-                            DoSummon(MOB_LIVE_TRAINEE, LiveTrigger1, 1);
-                        if (Creature *LiveTrigger2 = Unit::GetCreature(*me, LiveTriggerGUID[2]))
-                            DoSummon(MOB_LIVE_TRAINEE, LiveTrigger2, 1);
-                        break;
-                    }
-                    case MOB_LIVE_KNIGHT:
-                    {
-                        if (Creature *LiveTrigger3 = Unit::GetCreature(*me, LiveTriggerGUID[3]))
-                            DoSummon(MOB_LIVE_KNIGHT, LiveTrigger3, 1);
-                        if (Creature *LiveTrigger5 = Unit::GetCreature(*me, LiveTriggerGUID[5]))
-                            DoSummon(MOB_LIVE_KNIGHT, LiveTrigger5, 1);
-                        break;
-                    }
-                    case MOB_LIVE_RIDER:
-                    {
-                        if (Creature *LiveTrigger4 = Unit::GetCreature(*me, LiveTriggerGUID[4]))
-                            DoSummon(MOB_LIVE_RIDER, LiveTrigger4, 1);
-                        break;
-                    }
+                    if (Creature *LiveTrigger0 = Unit::GetCreature(*me, LiveTriggerGUID[4]))
+                        DoSummon(MOB_LIVE_TRAINEE, LiveTrigger0, 1);
+                    if (Creature *LiveTrigger1 = Unit::GetCreature(*me, LiveTriggerGUID[4]))
+                        DoSummon(MOB_LIVE_TRAINEE, LiveTrigger1, 1);
+                    break;
                 }
-            }
-            else
-            {
-                switch(entry)
+                case MOB_LIVE_KNIGHT:
                 {
-                    case MOB_LIVE_TRAINEE:
-                    {
-                        if (Creature *LiveTrigger0 = Unit::GetCreature(*me, LiveTriggerGUID[4]))
-                            DoSummon(MOB_LIVE_TRAINEE, LiveTrigger0, 1);
-                        if (Creature *LiveTrigger1 = Unit::GetCreature(*me, LiveTriggerGUID[4]))
-                            DoSummon(MOB_LIVE_TRAINEE, LiveTrigger1, 1);
-                        break;
-                    }
-                    case MOB_LIVE_KNIGHT:
-                    {
-                        if (Creature *LiveTrigger5 = Unit::GetCreature(*me, LiveTriggerGUID[4]))
-                            DoSummon(MOB_LIVE_KNIGHT, LiveTrigger5, 1);
-                        break;
-                    }
-                    case MOB_LIVE_RIDER:
-                    {
-                        if (Creature *LiveTrigger4 = Unit::GetCreature(*me, LiveTriggerGUID[4]))
-                            DoSummon(MOB_LIVE_RIDER, LiveTrigger4, 1);
-                        break;
-                    }
+                    if (Creature *LiveTrigger5 = Unit::GetCreature(*me, LiveTriggerGUID[4]))
+                        DoSummon(MOB_LIVE_KNIGHT, LiveTrigger5, 1);
+                    break;
+                }
+                case MOB_LIVE_RIDER:
+                {
+                    if (Creature *LiveTrigger4 = Unit::GetCreature(*me, LiveTriggerGUID[4]))
+                        DoSummon(MOB_LIVE_RIDER, LiveTrigger4, 1);
+                    break;
                 }
             }
         }
@@ -443,7 +410,7 @@ public:
                             DoScriptText(SAY_TELEPORT, me);
                             DoTeleportTo(PosGroundLiveSide);
                             me->SetReactState(REACT_AGGRESSIVE);
-                            me->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_OOC_NOT_ATTACKABLE);
+                            me->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_OOC_NOT_ATTACKABLE|UNIT_FLAG_NOT_SELECTABLE);
                             summons.DoAction(0, 0);
                             summons.DoZoneInCombat();
                             events.ScheduleEvent(EVENT_BOLT, 1000);
