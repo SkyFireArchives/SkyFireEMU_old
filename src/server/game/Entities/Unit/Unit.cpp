703f26791cd11a7f661aa11ad1f013e359ca3142
Rad3k <rad3k.t@seznam.cz>
2011-05-04 20:39:44 +0200

    Fixed Tricks of the Trade and some changes by me
    Thx Dr.Tenma

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -6283,6 +6283,18 @@ bool Unit::HandleDummyAuraProc(Unit *pVictim, uint32 damage, AuraEffect* trigger
                     triggered_spell_id = 32747;
                     break;
                 }
+				// Tricks of the Trade
+                case 57934:
+                {
+                    if (Unit* unitTarget = GetMisdirectionTarget())
+                    {
+                         RemoveAura(dummySpell->Id, GetGUID(), 0, AURA_REMOVE_BY_DEFAULT);
+                         CastSpell(this, 59628, true);
+                         CastSpell(unitTarget, 57933, true);
+                         return true;
+                     }
+                     return false;
+                 }
                 // Item - Druid T10 Balance 4P Bonus
                 case 70723:
                 {
