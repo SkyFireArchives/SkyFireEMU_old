21f76d184bd8c4b0b3cfb6f26c2419c421be413e
Rad3k <rad3k.t@seznam.cz>
2011-05-05 10:59:49 +0200

    Fixed bonus from dispell Vampirich Touch SQL

--- sql/updates/06_04_11_spell_ bonus_data.sql
+++ sql/updates/06_04_11_spell_ bonus_data.sql
@@ -1,3 +1,3 @@
 DELETE FROM spell_bonus_data WHERE entry = 64085;
 INSERT INTO spell_bonus_data VALUES
-(64085, 1.2, 0, 0, 0, "Priest - Vampiric Touch (Dispelled)");
+(64085, 1.2, -1, 0, 0, "Priest - Vampiric Touch (Dispelled)");
