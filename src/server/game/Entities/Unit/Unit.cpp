861603cd1ac99bc98386578b016708eb7ae48ecd
Rad3k <rad3k.t@seznam.cz>
2011-05-05 20:54:24 +0200

    Again implemented revert commit for fixed compile i rewrite some functions
    Implemented Rune Strike
    Implemented Impurity for gargoyle

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -10440,6 +10440,36 @@ uint32 Unit::SpellDamageBonus(Unit *pVictim, SpellEntry const *spellProto, uint3
                     if (pVictim->GetDiseasesByCaster(owner->GetGUID()) > 0)
                         DoneTotalMod *= (100.0f + aurEff->GetAmount()) / 100.0f;
 
+			// Rune Strike
+            if (spellProto->SpellFamilyFlags[1] & 0x20000000)
+                {
+                    float ApCoeffMod = 1.0f;
+                    // Impurity (dummy effect)
+                    if (GetTypeId() == TYPEID_PLAYER)
+                    {
+                        PlayerSpellMap playerSpells = this->ToPlayer()->GetSpellMap();
+                        for (PlayerSpellMap::const_iterator itr = playerSpells.begin(); itr != playerSpells.end(); ++itr)
+                        {
+                            if (itr->second->state == PLAYERSPELL_REMOVED || itr->second->disabled)
+                                continue;
+                            switch (itr->first)
+                            {
+                                case 49220:
+                                case 49633:
+                                case 49635:
+                                case 49636:
+                                case 49638:
+                                {
+                                    if (const SpellEntry *proto=sSpellStore.LookupEntry(itr->first))
+                                        AddPctN(ApCoeffMod, SpellMgr::CalculateSpellEffectAmount(proto, 0));
+                                }
+                                break;
+                            }
+                        }
+                    }
+                    DoneTotalMod += GetTotalAttackPowerValue(BASE_ATTACK) * 0.2 * ApCoeffMod;
+                }
+
             // Impurity (dummy effect)
             if (GetTypeId() == TYPEID_PLAYER)
             {
