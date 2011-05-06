843503221b5967b35751b1739624c225ab8b4369
ZenitH <alex.c2312@yahoo.com>
2011-05-06 20:39:35 +0300

    Removed useless SQL file

--- sql/updates/world_spell_proc_event_2_05_2011.sql
+++
@@ -1,5 +0,0 @@
--- Changes the SpellFamilyMasks to bigint for cataclysm spell flags
-
-ALTER TABLE `spell_proc_event` MODIFY COLUMN `SpellFamilyMask0` bigint;
-ALTER TABLE `spell_proc_event` MODIFY COLUMN `SpellFamilyMask1` bigint;
-ALTER TABLE `spell_proc_event` MODIFY COLUMN `SpellFamilyMask2` bigint;
