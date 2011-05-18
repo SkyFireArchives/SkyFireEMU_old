1aaee3324f45a74fecb6b98ac17c4898a9c4fb40
AriDEV <michalik.andrej@centrum.sk>
2011-05-18 22:48:55 +0200

    DB/Creature: Glenn Arbuckle <Gryphon Master> DB/Creature: Innkeeper Teresa <Innkeeper>
    Signed-off-by: AriDEV <michalik.andrej@centrum.sk>

---
+++ database/updates/18_05_11_innkeeper_teresa.sql
@@ -0,0 +1,3 @@
+-- Innkeeper Teresa <Innkeeper>
+UPDATE creature_template SET 
+minlevel=84, maxlevel=84, faction_A=12, faction_H=12, npcflag=66177, Health_mod=70946 ScriptName='npc_innkeeper' WHERE entry=49686;
