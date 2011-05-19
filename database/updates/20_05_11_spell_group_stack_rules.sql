1b041f83334b573f9dd9a12ab0dcfce5cf27d9d3
ZenitH <alex.c2312@yahoo.com>
2011-05-20 01:49:36 +0300

    Spells:Fixed Mark of the Wild correct raid buff

---
+++ database/updates/20_05_11_spell_group_stack_rules.sql
@@ -0,0 +1,6 @@
+-- Mark of the Wild stack fix
+-- by ZenitH
+
+INSERT INTO `spell_group` (`id`, `spell_id`) VALUES ('1251', '79060'), ('1251', '79061');
+
+INSERT INTO `spell_group_stack_rules` (`group_id`, `stack_rule`) VALUES ('1251', '2');
