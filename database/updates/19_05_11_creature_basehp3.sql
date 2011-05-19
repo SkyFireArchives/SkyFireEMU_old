9b156df190d233147034ef056ee60742d6e3e76e
AriDEV <michalik.andrej@centrum.sk>
2011-05-19 14:08:26 +0200

    DB/Creature: CLASS LEVEL - WARRIOR LEVEL 85 DB/Creature: CLASS LEVEL - WARRIOR LEVEL 84 DB/Creature: Ben Mora <Innkeeper> DB/Vendor: Ben Mora <Innkeeper> DB/Creature: Highbank Cannon DB/Creature: Highbank Cannoneer DB/Creature: Highbank Guardsman DB/Creature: Axebite Infantry DB/Creature: Highbank Prison Guard
    Signed-off-by: AriDEV <michalik.andrej@centrum.sk>

---
+++ database/updates/19_05_11_creature_basehp3.sql
@@ -0,0 +1,7 @@
+-- CREATURE CLASS LEVEL - WARRIOR LEVEL 85
+UPDATE creature_classlevelstats SET 
+basehp3=77490 WHERE level=85 AND class=1 AND basehp0=1 AND basehp1=1 AND basehp2=1 AND basehp3=1 AND basemana=0 AND basearmor=1;
+
+-- CREATURE CLASS LEVEL - WARRIOR LEVEL 84
+UPDATE creature_classlevelstats SET 
+basehp3=64496 WHERE level=84 AND class=1 AND basehp0=1 AND basehp1=1 AND basehp2=1 AND basehp3=0 AND basemana=0 AND basearmor=1;
