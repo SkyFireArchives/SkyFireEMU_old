5dd3e8ede970ef189e150889758f905697a3b1dd
ZenitH <alex.c2312@yahoo.com>
2011-05-22 19:30:50 +0800

    Spells : Consecration's correct triggerred spell

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -4036,7 +4036,7 @@ void SpellMgr::LoadSpellCustomAttr()
             ++count;
             break;
         case 26573 : //Consecration
-            spellInfo->EffectTriggerSpell[2] = 69930;
+            spellInfo->EffectTriggerSpell[2] = 82366;
             count++;
             break;
         case 25771: // Forbearance - wrong mechanic immunity in DBC since 3.0.x
