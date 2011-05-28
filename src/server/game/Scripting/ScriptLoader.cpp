4d4623f3d2d13cc02ac1c2b20b5367df9d2bf26b
Fronty <kevin.pleger@gmx.de>
2011-05-28 15:18:08 +0200

    Scripts/Icecrown Citadel: Implemented traps after Deathbringer Saurfang
    by Shauren. (cherry-picking from commit d49ed8263c9e901dfd2e392b96bfb43fcc36ee0c)

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -486,6 +486,7 @@ void AddSC_boss_blood_queen_lana_thel();
 void AddSC_boss_sindragosa();
 void AddSC_icecrown_citadel_teleport();
 void AddSC_instance_icecrown_citadel();
+void AddSC_icecrown_citadel();
 
 void AddSC_dalaran();
 void AddSC_borean_tundra();
@@ -1201,8 +1202,7 @@ void AddNorthrendScripts()
     AddSC_boss_sindragosa();
     AddSC_icecrown_citadel_teleport();
     AddSC_instance_icecrown_citadel();
-    AddSC_boss_sindragosa();
-    AddSC_boss_valithria_dreamwalker();
+    AddSC_icecrown_citadel();
 
     AddSC_dalaran();
     AddSC_borean_tundra();
