4fd7b093eefbb0d39297d0d207e77433afdd9021
Rad3k <rad3k.t@seznam.cz>
2011-05-30 21:51:48 +0800

    Fixed priest's talent Lightwell

--- src/server/scripts/World/npcs_special.cpp
+++ src/server/scripts/World/npcs_special.cpp
@@ -2028,27 +2028,6 @@ public:
     }
 };
 
-class npc_lightwell : public CreatureScript
-{
-public:
-    npc_lightwell() : CreatureScript("npc_lightwell") { }
-
-    struct npc_lightwellAI : public PassiveAI
-    {
-        npc_lightwellAI(Creature *c) : PassiveAI(c) {}
-
-        void Reset()
-        {
-            DoCast(me, 59907, false); // Spell for Lightwell Charges
-        }
-    };
-
-    CreatureAI *GetAI(Creature *creature) const
-    {
-        return new npc_lightwellAI(creature);
-    }
-};
-
 enum eTrainingDummy
 {
     NPC_ADVANCED_TARGET_DUMMY                  = 2674,
@@ -2935,7 +2914,6 @@ void AddSC_npcs_special()
     new npc_snake_trap;
     new npc_mirror_image;
     new npc_ebon_gargoyle;
-    new npc_lightwell;
     new mob_mojo;
     new npc_training_dummy;
     new npc_shadowfiend;
