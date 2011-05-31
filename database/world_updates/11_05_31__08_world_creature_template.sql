65bf86cef4883765eba21601100686c904895630
cooler-SAI <cooler19860212@yahoo.com>
2011-05-31 21:19:17 +0300

    DB/Creature: updated few creature_template in Elwynn Forest - no more Defias...

---
+++ database/world_updates/11_05_31__08_world_creature_template.sql
@@ -0,0 +1,8 @@
+UPDATE `creature_template` SET `name` = 'Cutpurse' WHERE `entry` = 94;
+UPDATE `creature_template` SET `name` = 'Bandit' WHERE `entry` = 116;
+UPDATE `creature_template` SET `name` = 'Dockworker' WHERE `entry` = 6927;
+UPDATE `creature_template` SET `name` = 'Bodyguard' WHERE `entry` = 6866;
+UPDATE `creature_template` SET `name` = 'Rogue Wizard' WHERE `entry` = 474;
+
+
+
