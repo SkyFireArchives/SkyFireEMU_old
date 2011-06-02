03b57079220a0405661c85f9861b9a92433ab694
AriDEV <michalik.andrej@centrum.sk>
2011-06-03 06:34:25 +0800

    DB/Creature: Felstalker (3102)
    Signed-off-by: AriDEV <michalik.andrej@centrum.sk>

---
+++ database/world_updates/11_06_02__05_world_creature_template.sql
@@ -0,0 +1,3 @@
+-- Felstalker
+UPDATE creature_template SET 
+npcflag=0, speed_walk=0.8, speed_run=1.14286, rangeattacktime=2178, dynamicflags=8, minrangedmg=2, maxrangedmg=3, rangedattackpower=100, InhabitType=1 WHERE entry=3102;
