8ed8b595c5c85f31b6211a824fd0e4c036a1e17f
Impas <kakaskar@yandex.ru>
2011-06-02 13:09:00 +0200

    repeated at fix crash in pet

--- src/server/game/Entities/Corpse/Corpse.cpp
+++ src/server/game/Entities/Corpse/Corpse.cpp
@@ -112,7 +112,7 @@ void Corpse::SaveToDB()
     DeleteFromDB(trans);
 
     std::ostringstream ss;
-    ss  << "INSERT INTO corpse (corpseGuid,guid,posX,posY,posZ,orientation,mapId,displayId,itemCache,bytes1,bytes2,guildId,flags,dynFlags,time,corpseType,instanceId,phaseMask) VALUES ("
+    ss  << "INSERT INTO corpse (corpseGuid, guid, posX, posY, posZ, orientation, mapId, displayId, itemCache, bytes1, bytes2, flags, dynFlags, time, corpseType, instanceId, phaseMask) VALUES ("
         << GetGUIDLow() << ", "
         << GUID_LOPART(GetOwnerGUID()) << ", "
         << GetPositionX() << ", "
