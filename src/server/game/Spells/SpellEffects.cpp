8971721beacd80a94602decde22f492cb3d13516
Dmitry Romanoff <aspro-wow@list.ru>
2011-06-03 18:48:23 +0800

    * Updated code style for Cone of Cold

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -1412,19 +1412,19 @@ void Spell::EffectDummy(SpellEffIndex effIndex)
             switch (m_spellInfo->Id)
             {
                 case 120: // Cone of Cold
+                {
+                    if (m_caster->GetTypeId() == TYPEID_PLAYER)
                     {
-                        if (m_caster->GetTypeId() == TYPEID_PLAYER)
-                            {
-                                if (m_caster->HasAura(11190)) // Improved Cone of Cold Rank 1
-                                {
-                                    m_caster->CastCustomSpell(unitTarget, 83301, &bp, NULL, NULL, true, 0);
-                                }
-                                if (m_caster->HasAura(12489)) // Improved Cone of Cold Rank 2
-                                {
-                                    m_caster->CastCustomSpell(unitTarget, 83302, &bp, NULL, NULL, true, 0);
-                                }
-                            }
+                        if (m_caster->HasAura(11190)) // Improved Cone of Cold Rank 1
+                        {
+                                m_caster->CastCustomSpell(unitTarget, 83301, &bp, NULL, NULL, true, 0);
+                        }
+                        if (m_caster->HasAura(12489)) // Improved Cone of Cold Rank 2
+                        {
+                            m_caster->CastCustomSpell(unitTarget, 83302, &bp, NULL, NULL, true, 0);
+                        }
                     }
+                }
                 case 1459: // Arcane Brilliance
                 {
                     if (m_caster->GetTypeId() == TYPEID_PLAYER)
