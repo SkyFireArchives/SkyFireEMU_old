335355e2b5634d0b10139298dc27adbba97d9df9
Impas <kakaskar@yandex.ru>
2011-06-04 22:52:51 +0200

    standard requirement players for bg

---
+++ sql/bg.sql
@@ -0,0 +1,14 @@
+REPLACE INTO `battleground_template` (`id`,`MinPlayersPerTeam`,`MaxPlayersPerTeam`,`MinLvl`,`MaxLvl`,`AllianceStartLoc`,`AllianceStartO`,`HordeStartLoc`,`HordeStartO`, `Weight`, `Comment`) VALUES
+(1,20,40,51,80,611,2.72532,610,2.27452,1,'Alterac Valley'),
+(2,5,10,10,80,769,3.14159,770,3.14159,1,'Warsong Gulch'),
+(3,8,15,20,80,890,3.40156,889,0.263892,1,'Arathi Basin'),
+(4,0,2,10,80,929,0,936,3.14159,1,'Nagrand Arena'),
+(5,0,2,10,80,939,0,940,3.14159,1,'Blades''s Edge Arena'),
+(6,0,2,10,80,0,0,0,0,1,'All Arena'),
+(7,8,15,61,80,1103,3.40156,1104,0.263892,1,'Eye of The Storm'),
+(8,0,2,10,80,1258,0,1259,3.14159,1,'Ruins of Lordaeron'),
+(9,7,15,71,80,1367,0,1368,0,1,'Strand of the Ancients'),
+(10,5,5,10,80,1362,0,1363,0,1,'Dalaran Sewers'),
+(11,5,5,10,80,1364,0,1365,0,1,'The Ring of Valor'),
+(30,20,40,71,80,1485,0,1486,0,1,'Isle of Conquest'),
+(32,10,10,0,80,0,0,0,0,1,'Random battleground');
