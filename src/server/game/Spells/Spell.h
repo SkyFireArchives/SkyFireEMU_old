e695a84157da8ef7e15231c5f06415310749b19d
Rad3k <rad3k.t@seznam.cz>
2011-06-10 00:05:24 +0200

    Fixed Shadow Damage for Scourge Strike

--- src/server/game/Spells/Spell.h
+++ src/server/game/Spells/Spell.h
@@ -623,8 +623,9 @@ class Spell
         GameObject* focusObject;
 
         // Damage and healing in effects need just calculate
-        int32 m_damage;           // Damage  in effects count here
-        int32 m_healing;          // Healing in effects count here
+        int32 m_damage;           // Damage        in effects count here
+        int32 m_healing;          // Healing      in effects count here
+        int32 m_final_damage;     // Final damage in effects count here
 
         //******************************************
         // Spell trigger system
