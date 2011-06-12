c2272f76e659bc46216d589782f7c55ec4f85b3a
Skyfire <admin@projectskyfire.org>
2011-06-12 12:39:51 -0500

    Some minor work on the auction house. It's not even close to being finished. Will work on the a bit more later. Family stuff to do today. Swiped some changes from Fabi too so thanks.
    Signed-off-by: Skyfire <admin@projectskyfire.org>

--- src/server/game/AuctionHouse/AuctionHouseMgr.h
+++ src/server/game/AuctionHouse/AuctionHouseMgr.h
@@ -29,7 +29,6 @@
 #include "DatabaseEnv.h"
 #include "DBCStructure.h"
 
-
 class Item;
 class Player;
 class WorldPacket;
@@ -58,12 +57,12 @@ struct AuctionEntry
     uint32 auctioneer;                                      // creature low guid
     uint32 item_guidlow;
     uint32 item_template;
-    uint32 owner;
-    uint32 startbid;                                        //maybe useless
-    uint32 bid;
+    uint64 owner;
+    uint64 startbid;                                        //maybe useless
+    uint64 bid;
     uint32 buyout;
     time_t expire_time;
-    uint32 bidder;
+    uint64 bidder;
     uint32 deposit;                                         //deposit can be calculated only when creating auction
     AuctionHouseEntry const* auctionHouseEntry;             // in AuctionHouse.dbc
     uint32 factionTemplateId;
@@ -72,7 +71,7 @@ struct AuctionEntry
     uint32 GetHouseId() const { return auctionHouseEntry->houseId; }
     uint32 GetHouseFaction() const { return auctionHouseEntry->faction; }
     uint32 GetAuctionCut() const;
-    uint32 GetAuctionOutBid() const;
+    uint64 GetAuctionOutBid() const;
     bool BuildAuctionInfo(WorldPacket & data) const;
     void DeleteFromDB(SQLTransaction& trans) const;
     void SaveToDB(SQLTransaction& trans) const;
@@ -151,7 +150,8 @@ public:
     void SendAuctionSalePendingMail(AuctionEntry * auction, SQLTransaction& trans);
     void SendAuctionSuccessfulMail(AuctionEntry * auction, SQLTransaction& trans);
     void SendAuctionExpiredMail(AuctionEntry * auction, SQLTransaction& trans);
-    void SendAuctionOutbiddedMail(AuctionEntry * auction, uint32 newPrice, Player* newBidder, SQLTransaction& trans);
+    void SendAuctionRemovedMail(AuctionEntry * auction, SQLTransaction& trans);
+    void SendAuctionOutbiddedMail(AuctionEntry * auction, uint64 newPrice, Player* newBidder, SQLTransaction& trans);
     void SendAuctionCancelledToBidderMail(AuctionEntry* auction, SQLTransaction& trans);
 
     static uint32 GetAuctionDeposit(AuctionHouseEntry const* entry, uint32 time, Item *pItem, uint32 count);
