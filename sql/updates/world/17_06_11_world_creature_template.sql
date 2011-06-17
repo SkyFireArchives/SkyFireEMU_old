3ccc8e6a971b06390bd997839ddccf1c794089ab
Zenith <alex.c2312@yahoo.com>
2011-06-18 00:49:52 +0300

    SQL/Scripts: Small update on sql updates date

---
+++ sql/updates/world/17_06_11_world_creature_template.sql
@@ -0,0 +1,13 @@
+-- Halls Of Origination
+
+UPDATE `creature_template` SET `ScriptName` = 'boss_temple_guardian_anhuur' WHERE `creature_template`.`entry` =39425;
+UPDATE `creature_template` SET `ScriptName` = 'boss_earthrager_ptah' WHERE `creature_template`.`entry` =39428;
+UPDATE `creature_template` SET `ScriptName` = 'boss_anraphet' WHERE `creature_template`.`entry` =39788;
+
+-- Lost City Of The Tol'vir
+
+UPDATE `creature_template` SET `ScriptName` = 'boss_general_husam' WHERE `creature_template`.`entry` =44577;
+UPDATE `creature_template` SET `ScriptName` = 'boss_lockmaw' WHERE `creature_template`.`entry` =43614;
+UPDATE `creature_template` SET `ScriptName` = 'boss_augh' WHERE `creature_template`.`entry` =49045;
+UPDATE `creature_template` SET `ScriptName` = 'boss_high_prophet_barim' WHERE `creature_template`.`entry` =43612;
+
