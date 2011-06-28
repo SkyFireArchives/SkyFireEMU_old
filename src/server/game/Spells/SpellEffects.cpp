9a34b5ee3bc39b8ffc4859a2f404f158f62211a3
Remake <havoc.remake@gmail.com>
2011-06-28 23:37:32 +0200

    Updated some code style.

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -512,34 +512,26 @@ void Spell::SpellDamageSchoolDmg(SpellEffIndex effIndex)
                    damage = uint32(damage * (m_caster->GetTotalAttackPowerValue(BASE_ATTACK)) / 100);
                // Victory Rush
                else if (m_spellInfo->SpellFamilyFlags[1] & 0x100)
-                damage = uint32(damage * m_caster->GetTotalAttackPowerValue(BASE_ATTACK) / 100);
+                   damage = uint32(damage * m_caster->GetTotalAttackPowerValue(BASE_ATTACK) / 100);
                // Cleave
                else if (m_spellInfo->Id == 845)
-               {
-               damage = uint32(6+ m_caster->GetTotalAttackPowerValue(BASE_ATTACK)* 0.45);
-               }
+                   damage = uint32(6+ m_caster->GetTotalAttackPowerValue(BASE_ATTACK)* 0.45);
                // Intercept
-                 else if (m_spellInfo->Id == 20253)
-                 {
-                 damage = uint32(1 + m_caster->GetTotalAttackPowerValue(BASE_ATTACK) * 0.12);
-                 }
+               else if (m_spellInfo->Id == 20253)
+                   damage = uint32(1 + m_caster->GetTotalAttackPowerValue(BASE_ATTACK) * 0.12);
                // Execute
                else if (m_spellInfo->Id ==5308)
-               {
-               damage = uint32 (10 + m_caster->GetTotalAttackPowerValue(BASE_ATTACK)* 0.437*100/100);  
-               }
+                   damage = uint32 (10 + m_caster->GetTotalAttackPowerValue(BASE_ATTACK)* 0.437*100/100);  
                // Heroic Strike
                else if (m_spellInfo->Id == 78)
-               {
-                damage = uint32(8 + m_caster->GetTotalAttackPowerValue(BASE_ATTACK)* 60 / 100);
-               }
+                   damage = uint32(8 + m_caster->GetTotalAttackPowerValue(BASE_ATTACK)* 60 / 100);
                // Shockwave
                else if (m_spellInfo->Id == 46968)
                {
-                int32 pct = m_caster->CalculateSpellDamage(unitTarget, m_spellInfo, 2);
-                if (pct > 0)
-                    damage+= int32(m_caster->GetTotalAttackPowerValue(BASE_ATTACK) * pct / 100);
-                    break;
+                   int32 pct = m_caster->CalculateSpellDamage(unitTarget, m_spellInfo, 2);
+                   if (pct > 0)
+                       damage+= int32(m_caster->GetTotalAttackPowerValue(BASE_ATTACK) * pct / 100);
+                   break;
                }
                break;
             }
