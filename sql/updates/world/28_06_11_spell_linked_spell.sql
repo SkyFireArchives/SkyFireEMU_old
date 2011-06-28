d08579090102ccb2c13bf346d25cf247d6e44c7b
Rad3k <rad3k.t@seznam.cz>
2011-06-28 02:03:31 +0200

    Fixed Shadow Sight

---
+++ sql/updates/world/28_06_11_spell_linked_spell.sql
@@ -0,0 +1,4 @@
+DELETE FROM `spell_linked_spell` WHERE `spell_trigger` = 34709;
+INSERT INTO `spell_linked_spell` VALUES
+(34709,-1784,1,'Shadow Sight - Stealth'),
+(34709,-5215,1,'Shadow Sight - Prowl');
--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -831,6 +831,7 @@ bool SpellMgr::_isPositiveEffect(uint32 spellId, uint32 effIndex, bool deep) con
             switch (spellId)
             {
                 case 34700: // Allergic Reaction
+                case 34709: // Shadow Sight
                 case 61987: // Avenging Wrath Marker
                 case 61988: // Divine Shield exclude aura
                     return false;
