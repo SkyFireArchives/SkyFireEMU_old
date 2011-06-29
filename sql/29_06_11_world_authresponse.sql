ec8f89a691d82f5aa6a072a5223c6c041329ae62
Chameleon <c-n@bk.ru>
2011-06-29 17:06:08 -0400

    Merged opcodes SQL updates into a single file for ease of application. Please add further changes to the same file.

--- sql/29_06_11_world_authresponse.sql
+++
@@ -1,1 +0,0 @@
-UPDATE emuopcodes SET number = 48891 WHERE name = "SMSG_AUTH_RESPONSE" AND version = 14333;
--- sql/29_06_11_world_char_enum.sql
+++
@@ -1,2 +0,0 @@
-UPDATE emuopcodes SET number = 50983 WHERE name = "SMSG_CHAR_ENUM" AND version = 14333;
-UPDATE emuopcodes SET number = 61990 WHERE name = "CMSG_CHAR_ENUM" AND version = 14333; 
