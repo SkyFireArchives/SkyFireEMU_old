606fd68fa2b652c324e231f98d152de13cbc0686
Dvlpr <coolguy@net.ru>
2011-06-30 00:33:17 +0400

    * More accurate sql updates. * Updated CMSG_CHAR_ENUM opcode. Do not attempt to connect - Crash/Freeze at char enum is guaranteed atm.

--- sql/29_06_11_world_authresponse.sql
+++ sql/29_06_11_world_authresponse.sql
@@ -1,1 +1,1 @@
-UPDATE emuopcodes SET number = 48891 WHERE name = "SMSG_AUTH_RESPONSE";
+UPDATE emuopcodes SET number = 48891 WHERE name = "SMSG_AUTH_RESPONSE" AND version = 14333;
--- sql/29_06_11_world_char_enum.sql
+++ sql/29_06_11_world_char_enum.sql
@@ -1,1 +1,2 @@
-UPDATE emuopcodes SET number = 50983 WHERE name = "SMSG_CHAR_ENUM";
+UPDATE emuopcodes SET number = 50983 WHERE name = "SMSG_CHAR_ENUM" AND version = 14333;
+UPDATE emuopcodes SET number = 61990 WHERE name = "CMSG_CHAR_ENUM" AND version = 14333; 
