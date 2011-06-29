156f7e474e9a3115307a98c3bea03ef4d2817a2d
cooler-SAI <cooler19860212@yahoo.com>
2011-06-29 19:35:56 +0300

    SQL: corrected format. NOTE to developers!!! please use this form for SQL files
    29_06_11__00_xxxxxxx and ofc pull before push! ^_^

--- sql/updates/world/28_06_11__01_world_spell_proc_event.sql
+++ sql/updates/world/28_06_11__01_world_spell_proc_event.sql
@@ -1,3 +1,3 @@
-DELETE FROM `spell_proc_event` WHERE `entry` = 32216;
-INSERT INTO `spell_proc_event` VALUES
-(32216,0,4,0,0x100,0,0x10,0,0,0,0);
+DELETE FROM `spell_proc_event` WHERE `entry` = 31834;
+INSERT INTO `spell_proc_event` (entry,SpellFamilyName,SpellFamilyMask0,procFlags) VALUES
+(31834,10,0x80000000,0x4000);
--- sql/updates/world/28_06_11__02_world_spell_proc_event.sql
+++
@@ -1,3 +0,0 @@
-DELETE FROM `spell_proc_event` WHERE `entry` = 31834;
-INSERT INTO `spell_proc_event` (entry,SpellFamilyName,SpellFamilyMask0,procFlags) VALUES
-(31834,10,0x80000000,0x4000);
