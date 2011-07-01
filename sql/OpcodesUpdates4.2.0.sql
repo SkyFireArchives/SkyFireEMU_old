809e4b25cb95e30183655c54fdaf0cbf15fef9f4
Dvlpr <coolguy@net.ru>
2011-07-01 15:26:17 +0400

    * Forgotten CMSG_CHAR_ENUM in the last commit.

--- sql/OpcodesUpdates4.2.0.sql
+++ sql/OpcodesUpdates4.2.0.sql
@@ -1,3 +1,4 @@
 SET @ver=14333;
 UPDATE emuopcodes SET number = 48891 WHERE name = "SMSG_AUTH_RESPONSE" and version = @ver;
 UPDATE emuopcodes SET number = 50983 WHERE name = "SMSG_CHAR_ENUM" and version = @ver;
+UPDATE emuopcodes SET number = 61990 WHERE name = "CMSG_CHAR_ENUM" and version = @ver;
