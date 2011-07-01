01647ce3093412f76f1be14d8ef6b1bfd88a3c39
Chameleon <c-n@bk.ru>
2011-07-01 01:33:41 -0400

    CMSG_PLAYER_LOGIN opcode

--- sql/OpcodesUpdates4.2.0.sql
+++ sql/OpcodesUpdates4.2.0.sql
@@ -1,3 +1,5 @@
 SET @ver=14333;
 UPDATE emuopcodes SET number = 48891 WHERE name = "SMSG_AUTH_RESPONSE" and version = @ver;
 UPDATE emuopcodes SET number = 50983 WHERE name = "SMSG_CHAR_ENUM" and version = @ver;
+UPDATE emuopcodes SET number = 4305 WHERE name = "CMSG_PLAYER_LOGIN" and version = @ver;
+
