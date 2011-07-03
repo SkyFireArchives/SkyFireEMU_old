da333dd4d7f298ec38109df4bb8f72ead40ed54e
Skyfire <admin@projectskyfire.org>
2011-07-02 21:11:47 -0500

    update the auth realmlist to revision 14333. This is a work in progress and this branch is not usable.\
    Signed-off-by: Skyfire <admin@projectskyfire.org>

--- sql/base/auth/auth.sql
+++ sql/base/auth/auth.sql
@@ -116,14 +116,14 @@ CREATE TABLE `realmlist` (
   `allowedSecurityLevel` tinyint(3) unsigned NOT NULL DEFAULT '0',
   `population` float unsigned NOT NULL DEFAULT '0',
   `online` int(11) NOT NULL DEFAULT '0',
-  `gamebuild` int(11) unsigned NOT NULL DEFAULT '13623',
+  `gamebuild` int(11) unsigned NOT NULL DEFAULT '14333',
   PRIMARY KEY (`id`),
   UNIQUE KEY `idx_name` (`name`)
 ) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Realm System';
 
 LOCK TABLES `realmlist` WRITE;
 /*!40000 ALTER TABLE `realmlist` DISABLE KEYS */;
-INSERT INTO `realmlist` VALUES (1,'SkyFireEMU','127.0.0.1',8085,0,0,1,0,0,0,13623);
+INSERT INTO `realmlist` VALUES (1,'SkyFireEMU','127.0.0.1',8085,0,0,1,0,0,0,14333);
 /*!40000 ALTER TABLE `realmlist` ENABLE KEYS */;
 UNLOCK TABLES;
 DROP TABLE IF EXISTS `uptime`;
