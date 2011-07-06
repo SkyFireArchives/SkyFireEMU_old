d5b989207ace65dd640af0e1608dbc31bf062f03
MobileDev <Stage6@EMPulsegaming.com>
2011-07-06 10:07:23 -0500

    Core: get rid of the revision-id. - it's not useful in a DVCS at all original patch by Click
    Signed-off-by: MobileDev <Stage6@EMPulsegaming.com>

--- cmake/showoptions.cmake
+++ cmake/showoptions.cmake
@@ -1,6 +1,6 @@
 # output generic information about the core and buildtype chosen
-
-message("* SkyFireEMU revision   : ${rev_id_str} (${rev_hash_str})")
+message("")
+message("* SkyFireEMU commithash : ${rev_hash_str}")
 if( UNIX )
   message("* Build binaries in      : ${CMAKE_BUILD_TYPE} mode")
 endif()
