7c185b91de69f87cccca78dc7e702264b6cba3a8
Bootz <Stage6Dev@EMPulsegaming.com>
2011-08-07 23:20:01 -0500

    Repo: removing the binaries that should not of gotten pushed... MEDIC! we've a man down!! WTB some fail less
    Signed-off-by: Bootz <Stage6Dev@EMPulsegaming.com>

--- NEW/revision.h
+++
@@ -1,9 +0,0 @@
-#ifndef __REVISION_H__
-#define __REVISION_H__
- #define _REVISION      "0"
- #define _HASH          "Archive"
- #define FILEVER        0,0,0,0
- #define PRODUCTVER     0,0,0,0
- #define STRFILEVER     "0, 0, 0, 0"
- #define STRPRODUCTVER  "0, 0, 0, 0"
-#endif // __REVISION_H__
