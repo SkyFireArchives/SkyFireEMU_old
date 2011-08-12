ad5f81fef2c45cf09e5c0919ab7c3e26e560a9fb
Sovak <hacker007@zoznam.sk>
2011-08-12 18:07:18 +0200

    Fixed crash when summoning player from vehicle
    Signed-off-by: Sovak <hacker007@zoznam.sk>

--- src/server/game/Chat/Commands/Level1.cpp
+++ src/server/game/Chat/Commands/Level1.cpp
@@ -515,6 +515,9 @@ bool ChatHandler::HandleSummonCommand(const char* args)
 
         Map* pMap = m_session->GetPlayer()->GetMap();
 
+        if (target->GetVehicle())
+            target->ExitVehicle();
+
         if (pMap->IsBattlegroundOrArena())
         {
             // only allow if gm mode is on
