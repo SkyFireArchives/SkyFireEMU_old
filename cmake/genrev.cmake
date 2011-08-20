4c4bc18bb262a0aa8a654cb40a7fadeb0dd6fc76
Bootz <Stage6@EMPulsegaming.com>
2011-08-20 17:03:18 -0500

    Core: get rid of the revision-id. - it's not useful in a DVCS at all original patch by Click
    Signed-off-by: MobileDev <Stage6@EMPulsegaming.com>
    Signed-off-by: Bootz <Stage6Dev@EMPulseGaming.com>

--- cmake/genrev.cmake
+++ cmake/genrev.cmake
@@ -8,40 +8,64 @@
 # WITHOUT ANY WARRANTY, to the extent permitted by law; without even the
 # implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
-execute_process(
-  COMMAND git log --pretty=format:"" origin/master
-  COMMAND wc -l
-  WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
-  OUTPUT_VARIABLE rev_id_str
-  OUTPUT_STRIP_TRAILING_WHITESPACE
-  ERROR_QUIET
+include(${CMAKE_SOURCE_DIR}/cmake/macros/EnsureVersion.cmake)
+
+set(_REQUIRED_GIT_VERSION "1.7")
+
+find_program(_GIT_EXEC
+  NAMES
+    git git.cmd
+  HINTS
+    ENV PATH
+  DOC "git installation path"
 )
 
-execute_process(
-  COMMAND git rev-parse --short=12 HEAD
-  WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
-  OUTPUT_VARIABLE rev_hash_str
-  OUTPUT_STRIP_TRAILING_WHITESPACE
-  ERROR_QUIET
-)
+if(_GIT_EXEC)
+  execute_process(
+    COMMAND "${_GIT_EXEC}" --version
+    COMMAND git log --pretty=format:"" origin/master
+    COMMAND wc -l    
+	WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
+    OUTPUT_VARIABLE _GIT_VERSION
+    ERROR_QUIET
+  )
+
+  # make sure we're using minimum the required version of git, so the "dirty-testing" will work properly
+  ensure_version( "${_REQUIRED_GIT_VERSION}" "${_GIT_VERSION}" _GIT_VERSION_OK)
+endif()
+
+if(_GIT_VERSION_OK)
+  execute_process(
+    COMMAND "${_GIT_EXEC}" describe --match init --dirty=+ --abbrev=12
+    COMMAND git rev-parse --short=12 HEAD
+	WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
+    OUTPUT_VARIABLE rev_info
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    ERROR_QUIET
+  )
+else()
+  message("")
+  message(STATUS "WARNING - Missing or outdated git - did you forget to install a recent version?")
+  message(STATUS "WARNING - Observe that for revision ID/hash to work you need at least version ${_REQUIRED_GIT_VERSION}")
+  message(STATUS "WARNING - Continuing anyway, but setting the revision-ID and hash to Rev:0 Hash: Archive")
+  message("")
+endif()
 
 # Last minute check - ensure that we have a proper revision
 # If everything above fails (means the user has erased the git revision control directory or removed the origin/HEAD tag)
-if(NOT rev_id_str)
+if(NOT rev_info)
   message("")
   message(STATUS "WARNING - No revision-information found - have you been tampering with the sources?")
 
   # Ok, since we have no valid ways of finding/setting the revision, let's force some defaults
   set(rev_hash_str "Archive")
   set(rev_hash "0")
-  set(rev_id_str "0")
-  set(rev_id "0")
 else()
- string(STRIP ${rev_hash_str} rev_hash_str)
- set(rev_hash ${rev_hash_str})
- string(STRIP ${rev_id_str} rev_id_str)
- set(rev_id ${rev_id_str})
+  # Extract revision and hash from git
+  string(REGEX REPLACE init-|[0-9]+-g "" rev_hash_str ${rev_info})
+  string(REGEX REPLACE [+]+ "" rev_hash ${rev_hash_str})
 endif()
+
 
 # Its not set during initial run
 if(NOT BUILDDIR)
@@ -49,11 +73,11 @@ if(NOT BUILDDIR)
 endif()
 
 # Create the actual revision.h file from the above params
-if(NOT "${rev_id_cached}" MATCHES "${rev_id_str}")
+if(NOT "${rev_hash_cached}" MATCHES "${rev_hash_str}")
   configure_file(
     "${CMAKE_SOURCE_DIR}/revision.h.in.cmake"
     "${BUILDDIR}/revision.h"
     @ONLY
   )
-  set(rev_id_cached "${rev_id_str}" CACHE INTERNAL "Cached revision ID")
+  set(rev_hash_cached "${rev_hash_str}" CACHE INTERNAL "Cached commit-hash")
 endif()
