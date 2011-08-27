e662d67cc6bc02fac28158e4f79624f761f387d5
Bootz <Stage6Dev@EMPulseGaming.com>
2011-08-26 23:33:47 -0500

    Scripts/Spells: Fixed some spellscripts... more corrections   , some left out, and some adjustments for cata.

--- src/server/scripts/Spells/spell_druid.cpp
+++ src/server/scripts/Spells/spell_druid.cpp
@@ -1,6 +1,7 @@
 /*
- * Copyright (C) 2008-2010 TrinityCore <http://www.trinitycore.org/>
- * Copyright (C) 2010-2011 MigCore <http://wow-mig.ru/>
+ * Copyright (C) 2010-2011 Project SkyFire <http://www.projectskyfire.org/> 
+ * Copyright (C) 2008-2011 TrinityCore <http://www.trinitycore.org/>
+ * Copyright (C) 2005-2009 MaNGOS <http://getmangos.com/>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
@@ -15,7 +16,7 @@
  * You should have received a copy of the GNU General Public License along
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
+ 
 /*
  * Scripts for spells with SPELLFAMILY_DRUID and SPELLFAMILY_GENERIC spells used by druid players.
  * Ordered alphabetically using scriptname.
