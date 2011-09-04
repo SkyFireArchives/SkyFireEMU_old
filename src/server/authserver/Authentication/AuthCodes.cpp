c2e70183fd0bf2133c25b5439f4d4a5fa9ca850c
Skyfire <admin@projectskyfire.org>
2011-09-04 00:19:06 -0500

    Reverted some of the auth server changes. Will clean this up on the next commit. You should now be able to authenticate with a pre cata client. /slap Bootz.
    Signed-off-by: Skyfire <admin@projectskyfire.org>

--- src/server/authserver/Authentication/AuthCodes.cpp
+++ src/server/authserver/Authentication/AuthCodes.cpp
@@ -1,42 +1,56 @@
 /*
+ * Copyright (C) 2005-2011 MaNGOS <http://www.getmangos.com/>
+ *
+ * Copyright (C) 2008-2011 Trinity <http://www.trinitycore.org/>
+ *
  * Copyright (C) 2010-2011 Project SkyFire <http://www.projectskyfire.org/>
- * Copyright (C) 2008-2011 TrinityCore <http://www.trinitycore.org/>
- * Copyright (C) 2005-2009 MaNGOS <http://getmangos.com/>
  *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2 of the License, or (at your
- * option) any later version.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along
- * with this program. If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
 #include "AuthCodes.h"
 
 namespace AuthHelper
 {
-    bool IsPreCataAcceptedClientBuild(int build)
+    bool IsPreBCAcceptedClientBuild(int build)
     {
-        int accepted_versions[] = PRE_CATA_ACCEPTED_CLIENT_BUILD;
+        int accepted_versions[] = PRE_BC_ACCEPTED_CLIENT_BUILD;
 
-        for (int i = 0; accepted_versions[i]; ++i)
+		for (int i = 0; accepted_versions[i]; ++i)
+            if (build == accepted_versions[i])
+                return true;
+
+		return false;
+    }
+
+    bool IsPostBCAcceptedClientBuild(int build)
+    {
+        int accepted_versions[] = POST_BC_ACCEPTED_CLIENT_BUILD;
+
+		for (int i = 0; accepted_versions[i]; ++i)
             if (build == accepted_versions[i])
                 return true;
 
         return false;
     }
 
-    bool IsPostCataAcceptedClientBuild(int build)
+    bool IsPostWotLKAcceptedClientBuild(int build)
     {
-        int accepted_versions[] = POST_CATA_ACCEPTED_CLIENT_BUILD;
+        int accepted_versions[] = POST_WOTLK_ACCEPTED_CLIENT_BUILD;
 
-        for (int i = 0; accepted_versions[i]; ++i)
+		for (int i = 0; accepted_versions[i]; ++i)
             if (build == accepted_versions[i])
                 return true;
 
@@ -45,6 +59,6 @@ namespace AuthHelper
 
     bool IsAcceptedClientBuild(int build)
     {
-        return (IsPostCataAcceptedClientBuild(build) || IsPreCataAcceptedClientBuild(build));
+        return (IsPostWotLKAcceptedClientBuild(build) || IsPostBCAcceptedClientBuild(build) || IsPreBCAcceptedClientBuild(build));
     }
 };
