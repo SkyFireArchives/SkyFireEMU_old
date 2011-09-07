a1d25a00bdb152ac271da92e4f56d3820b86b4e2
Skyfire <admin@projectskyfire.org>
2011-09-06 23:12:06 -0500

    Core/Spell System. Fix SPELL_AURA_PERIODIC_HEAL. Patch by E.T. http://www.projectskyfire.org/index.php/topic/4041-fix-spell-aura-periodic-heal/
    Signed-off-by: Skyfire <admin@projectskyfire.org>

--- src/server/game/Spells/Auras/SpellAuraEffects.cpp
+++ src/server/game/Spells/Auras/SpellAuraEffects.cpp
@@ -1753,10 +1753,9 @@ void AuraEffect::PeriodicTick(AuraApplication * aurApp, Unit * caster) const
             else
             {
                 // Wild Growth = amount + (6 - 2*doneTicks) * ticks* amount / 100
+                int32 addition = 0;
                 if (m_spellProto->SpellFamilyName == SPELLFAMILY_DRUID && m_spellProto->SpellIconID == 2864)
-                    damage += int32(float(damage * GetTotalTicks()) * ((6 - float(2 * (GetTickNumber() - 1))) / 100));
-
-                int32 addition = int32(float(damage * GetTotalTicks()) * ((6-float(2*(GetTickNumber()-1)))/100));
+                    addition = int32(float(damage * GetTotalTicks()) * ((6-float(2*(GetTickNumber()-1)))/100));
 
                 // Item - Druid T10 Restoration 2P Bonus
                 if (AuraEffect * aurEff = caster->GetAuraEffect(70658, 0))
