3b923bca42d975bdfd1c0c546210a35932407d17
wlasser <lostbyte32@gmail.com>
2011-12-02 14:03:16 +0400

    Fix drain life for warlocks. wlasser and rebase project. SQL: DELETE FROM spell_script_names WHERE spell_id = 689 OR spell_id = -689;
    INSERT INTO spell_script_names (spell_id,scriptname) VALUES (689,'spell_warl_drain_life');

--- src/server/scripts/Spells/spell_warlock.cpp
+++ src/server/scripts/Spells/spell_warlock.cpp
@@ -378,6 +378,40 @@ public:
         return new spell_warl_shadow_bite_SpellScript();
     }
 };
+//DrainLife fix: thnks to rebase. fixed by wlasser 
+class spell_warl_drain_life : public SpellScriptLoader
+
+{
+
+public:
+
+    spell_warl_drain_life() : SpellScriptLoader("spell_warl_drain_life") { }
+    class spell_warl_drain_life_AuraScript : public AuraScript
+
+    {
+        PrepareAuraScript(spell_warl_drain_life_AuraScript);
+        void OnPeriodic(AuraEffect const* /*aurEff*/)
+       {
+          int32 bp = 2; // Normal, restore 2% of health
+         // Check for Death's Embrace
+
+  if(AuraEffect const* aurEff = GetCaster()->GetAuraEffect(SPELL_AURA_DUMMY, SPELLFAMILY_WARLOCK, 3223, 0))
+           if(GetCaster()->HealthBelowPct(25))
+                 bp += int32(aurEff->GetAmount());
+            GetCaster()->CastCustomSpell(GetCaster(), 89653, &bp, NULL, NULL, true);
+        }
+        void Register()
+
+    {
+         OnEffectPeriodic += AuraEffectPeriodicFn(spell_warl_drain_life_AuraScript::OnPeriodic, EFFECT_0, SPELL_AURA_PERIODIC_DAMAGE);
+ }
+ };
+        AuraScript* GetAuraScript() const
+ {
+         return new spell_warl_drain_life_AuraScript();
+         }
+};
+
 
 void AddSC_warlock_spell_scripts()
 {
@@ -389,4 +423,5 @@ void AddSC_warlock_spell_scripts()
     new spell_warl_immolate();
     new spell_warl_seed_of_corruption();
     new spell_warl_shadow_bite();
+    new spell_warl_drain_life();
 }
