51aa5ee7fe0be2f99ace663fa7bf9ed452a7b627
SkyFire <admin@projectskyfire.org>
2011-12-30 18:58:12 -0600

    Update all the cmake files with uniform copyright. Also updated to 2012 copyrights.
    Signed-off-by: SkyFire <admin@projectskyfire.org>

--- revision.h.in.cmake
+++ revision.h.in.cmake
@@ -3,7 +3,7 @@
  #define _HASH                      "@rev_hash@"
  #define _DATE                      "@rev_date@"
  #define VER_COMPANYNAME_STR        "SkyFire Emulation Developers"
- #define VER_LEGALCOPYRIGHT_STR     "(c)2010-2011 SkyFireEMU"
+ #define VER_LEGALCOPYRIGHT_STR     "(c)2010-2012 SkyFireEMU"
  #define VER_FILEVERSION            0,0,0
  #define VER_FILEVERSION_STR        "@rev_date@ (@rev_hash@)"
  #define VER_PRODUCTVERSION         VER_FILEVERSION
